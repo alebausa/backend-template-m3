dc552ece410074552ee804c8ac625d64d7635e4a
Ale Bausa <ale.bausa@gmail.com>
2022-09-20 15:24:41 +0200

    deploy

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,9 @@
 const router = require('express').Router();
-const { isAuthenticated, isAdmin } = require('../middlewares/jwt');
 
 // @desc    Index page for the API
 // @route   GET /api/v1/
 // @access  Public
-router.get('/', isAuthenticated, isAdmin, async (req, res, next) => {;
+router.get('/', async (req, res, next) => {;
   res.send('REST API')
 });
 
